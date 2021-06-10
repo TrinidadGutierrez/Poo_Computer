e2d286dfd85130583bfab387bf3d616e5253967c
tveronica <tveronica@TrinidadPc>
2021-06-09 23:09:06 -0300

    Agregacion de Variables

---
+++ src/JavaProject.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class JavaProject {
+
+
+
+
+static final Scanner input = new Scanner(System.in);
+public static void main(String[] args) {
+	System.out.println(" Hello! and Welcome ");
+	
+	String nombre = " Trinidad";
+	}
+}
